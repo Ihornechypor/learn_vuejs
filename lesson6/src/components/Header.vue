62555a67ca6a236067d85715f585940032ed8dd0
ihor.nechypor <ihor.nechypor@laurenscoster.com>
2019-04-01 16:53:44 +0200

    lesson6

---
+++ lesson6/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="">
+    <AppCart></AppCart>
+    <br>
+    <button type="button" name="order"
+            :disabled="btnDisabled"
+            @click="orderClick"
+    >
+    order
+    </button><br>
+    <div v-show="order">
+      sdd
+    </div>
+  </div>
+
+</template>
+
+<script>
+import AppCart from './Cart';
+import {mapGetters} from 'vuex';
+import {mapMutations} from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters([
+      'cnt',
+      'order'
+    ]),
+    btnDisabled(){
+      return this.cnt < 1;
+    }
+  },
+  methods: {
+    ...mapMutations([
+      'orderClick'
+    ])
+  },
+  components: {
+    AppCart
+  }
+
+}
+</script>
---
+++ lesson6/src/store/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+    state: {
+      cnt: 0,
+      price: 1000,
+      order: false
+    },
+    getters: {
+      cnt(state){
+        return state.cnt
+      },
+      price(state){
+        return state.price
+      },
+      total(state){
+        return state.cnt * state.price
+      },
+      order(state){
+          return state.order
+      }
+    },
+    mutations: {
+      minus(state){
+        if(state.cnt > 0) {
+          state.cnt--;
+        }
+      },
+      plus(state){
+        state.cnt++;
+      },
+      orderClick(state){
+        state.order = true;
+      }
+    },
+    actions: {
+      sendOrder(store){
+        store.commit('orderClick');
+        setTimeout()
+      }
+
+    }
+    strict: process.env.NODE_EBN !== 'prodaction'
+});
