e9940152c342fb32995ce6a0d5427fc698edde94
IhorNechypor <chip_a@ukr.net>
2019-04-22 09:12:50 +0200

    add btn

--- lesson7/leson_src_ih/lesson7/src/components/Product.vue
+++ lesson7/leson_src_ih/lesson7/src/components/Product.vue
@@ -3,30 +3,60 @@
     <h3>Product name: </h3>
     {{id}}
     <br>
-    {{product.title}}
+    {{product.name}}
+    <br>
     {{product.price}}
+    <br>
     <router-link :to="{ name: 'products'}">
       back to products
     </router-link>
+    <br>
     <!-- add to cart homework  -->
-
+    <button v-if="inCart.indexOf(product.id_prod) === -1"
+      type="button"
+      name="add"
+      @click="addToCart(product.id_prod)"
+      >
+      add
+    </button>
+    <button v-else
+      type="button"
+      name="add2"
+      @click="removeCart(product.id_prod)"
+      >
+      delite
+    </button>
   </div>
 
 </template>
 
 <script>
 
-
+import {mapGetters} from 'vuex';
+import {mapActions} from 'vuex';
 export default {
+
+
   computed: {
+    ...mapGetters('products',{
+      products: 'items'
+    }),
+    ...mapGetters('cart',{
+      inCart: 'products'
+    }),
     id(){
       return this.$route.params.id
     },
     product(){
       return this.$store.getters['products/item'](this.id);
     }
-
-}
+  },
+  methods: {
+    ...mapActions('cart',{
+      addToCart: 'add',
+      removeCart: 'remove'
+    })
+  }
 
 }
 </script>
--- lesson7/leson_src_ih/lesson7/src/store/index.js
+++ lesson7/leson_src_ih/lesson7/src/store/index.js
@@ -6,12 +6,14 @@ Vue.use(Vuex);
 import cart from './modules/cart';
 import menu from './modules/menu';
 import products from './modules/products';
+import checkout from './modules/checkout';
 
 export const store = new Vuex.Store({
   modules: {
     cart,
     menu,
-    products
+    products,
+    checkout
   },
   strict: process.env.NODE_EBN !== 'prodaction'
 });
